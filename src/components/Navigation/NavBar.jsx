6cdcad0df52e8fac2340db89734d80c0e96f2907
Asus <anuradh-se19049@stu.kln.ac.lk>
2023-09-28 16:39:06 +0530

    Add properties to navbar

---
+++ src/components/Navigation/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NavBar.css'
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" />
+                    Company Name
+                </a> About Session
+                <a className="navbar-brand center-title" href="#">
+                    FeedBack Form
+                </a>
+            </nav>
+        </div>
+    )
+}
