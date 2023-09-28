5544e6bdadbc6854ebbf73749a4030e688f42fd8
Asus <anuradh-se19049@stu.kln.ac.lk>
2023-09-28 19:11:07 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import NavBar from './components/Navigation/NavBar';
 import FeedBackForm from './pages/FeedBack/FeedBackForm';
+import { Route, Routes } from "react-router-dom";
+import About from './pages/About/About';
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      <FeedBackForm/>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<About />} />
+        <Route path="/feedback" element={<FeedBackForm />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import './NavBar.css'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
     return (
         <div>
-            <nav class="navbar navbar-light bg-light">
-                <a class="navbar-brand" href="#">
-                    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" />
-                    Company Name
-                </a> About Session
-                <a className="navbar-brand center-title" href="#">
-                    FeedBack Form
-                </a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Compnay Name</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className='nav-link' to={"/"}>About Session</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className='nav-link' to={"/feedback"}>Feed Back Form</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     )
